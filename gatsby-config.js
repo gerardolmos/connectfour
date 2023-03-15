f1c3089e5a533f340bc1e441063bd8ec6d0bacde
Gerard Olmos <gerardolmos@Mac-mini-de-Gerard.local>
2023-03-15 14:45:03 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `cuatroenraya`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
